0e069303c107d3eece596bd0032fac8aaa1bfa57
Scott Rinaberger <scottrinaberger@gmail.com>
2022-09-27 10:40:58 -0500

    push code to create or post data

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const fs = require('fs');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data (tells server how to use incoming data) (needs to be up here and just below app on line 5)
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -36,8 +43,35 @@ function findById(id, animalsArray) {
   return result;
 }
 
+//creates new data function
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
 
-//creates the route to the data & links query to data
+//validating data
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+//creates the route to the data & links query to data/RETRIEVAL of DATA
 app.get('/api/animals', (req, res) => {
   let results = animals;
   if (req.query) {
@@ -53,6 +87,20 @@ app.get('/api/animals/:id', (req, res) => {
   } else {
     res.send(404);
   }
+});
+
+//Create/POST function
+app.post('/api/animals', (req, res) => {  
+ // set id based on what the next index of the array will be
+ req.body.id = animals.length.toString(); //adds one more to id
+// add animal to json file and animals array in this function
+// if any data in req.body is incorrect, send 400 error back
+if (!validateAnimal(req.body)) {
+  res.status(400).send('The animal is not properly formatted.');
+} else {
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+}
 });
 
 app.listen(PORT, () => {
